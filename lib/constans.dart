bd5b11d789b2a030dc34b274d6632fd7f80de0ed
EmirhanT <emirhantokatt@gmail.com>
2023-07-08 11:07:05 +0300

    .

--- lib/constans.dart
+++ lib/constans.dart
@@ -3,6 +3,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/controllers/auth_controller.dart';
+import 'package:tiktok_clone/views/widgets/screens/add_video_screen.dart';
+
+
+const pages = [
+  Text("Home Screen"),
+  Text("Search Screen"),
+  AddVideoScreen(),
+  Text("Messages Screen"),
+  Text("Profile Screen"),
+];
 
 //Colors
 const backgroundColor = Colors.black;
--- lib/views/widgets/screens/home_screen.dart
+++ lib/views/widgets/screens/home_screen.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constans.dart';
+import 'package:tiktok_clone/views/widgets/custom_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIdx = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (idx){
+          setState(() {
+            pageIdx = idx;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        currentIndex: pageIdx,
+        items: const[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 30,
+            ),
+            label: "Home",
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              size: 30,
+            ),
+            label: "Search",
+          ),
+           BottomNavigationBarItem(
+            icon: CustomIcon(),
+              label: "",
+            ),
+           BottomNavigationBarItem(
+            icon: Icon(
+              Icons.message,
+              size: 30,
+            ),
+            label: "Message",
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              size: 30,
+            ),
+            label: "Profile",
+          ),
+        ],
       ),
+      body: pages[pageIdx],
     );
   }
 }
